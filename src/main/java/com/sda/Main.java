1018d119313559845f3871ac41646a7e2ca8aaa7
Feniks <shehufeniks@gmail.com>
2024-02-11 10:08:09 +0200

    [add] - Person class created

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,7 +1,12 @@
 package com.sda;
 
+import model.Person;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Person me = new Person("Feniks", "Shehu", 23);
+        System.out.println("Hi, it's me: " + me);
     }
 }
