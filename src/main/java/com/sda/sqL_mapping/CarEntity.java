47181d594771984fa30b9c6ae9a954dadae1d371
Feniks <shehufeniks@gmail.com>
2024-03-16 11:01:57 +0200

    [change] - java sql example

--- src/main/java/com/sda/sqL_mapping/CarEntity.java
+++ src/main/java/com/sda/sqL_mapping/CarEntity.java
@@ -1,4 +1,18 @@
 package com.sda.sqL_mapping;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarEntity {
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
 }
---
+++ src/main/java/com/sda/sqL_mapping/DbOperations.java
@@ -0,0 +1,44 @@
+package com.sda.sqL_mapping;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DbOperations {
+    // CRUD
+
+    // Read all / Select
+    public List<PersonEntity> readAllPersons(){
+        // TODO
+        return Collections.emptyList();
+    }
+
+    public PersonEntity readById(Long personId){
+        // TODO
+        return null;
+    }
+
+    // Create / Insert clause
+    public PersonEntity savePerson(PersonEntity personToSave){
+        // personToSave without id
+
+        // return person with update id
+        // TODO
+        return null;
+    }
+
+    //Delete / Delete clause
+    public boolean deletePersonById(Long id) {
+        //id of person to delete
+        // return:
+        //true - object was deleted
+        //false - object wasn't deleted
+        //TODO
+        return false;
+    }
+
+    //Update / Update Clause
+    public PersonEntity updatePerson(PersonEntity toUpdate){
+        //TODO
+        return null;
+    }
+}
